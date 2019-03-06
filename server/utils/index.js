25fe925ce96d2ec76fa7d8e5f7ccfd62bbcd762b
Kazachenko A <badabum0001@gmail.com>
2019-03-07 00:21:15 +0300

    delete

--- server/utils/index.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-exports.createHash = (text = Math.random(1000 * 1000 * 1000)) => {
-    return crypto.createHmac('sha1', process.env.SECRET_KEY).update(text).digest('hex');
-}
